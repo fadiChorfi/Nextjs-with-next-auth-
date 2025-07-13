69ec076fafa245046cbde3ff4f573398026d9f59
fadydodje <fadydodje@gmail.com>
2025-07-13 19:13:27 +0100

    fix: fixed typo error

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-    if ()
+    
   return (
     <html lang="en">
       <body>{children}</body>
